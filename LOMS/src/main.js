758edd280d90912bf13ef06b782c1c27fec5b866
tyrealgray <tyrealgray@ymail.com>
2017-05-14 00:01:30 +0200

    no need to set scale
    Former-commit-id: b8a6018aa9629f9c2c5d98ef0fbc5289c253e38c

--- LOMS/src/main.js
+++ LOMS/src/main.js
@@ -11,9 +11,6 @@ mainRenderer.addResource('logo','./LOMS.png',(logo)=>{
     sprite.position.x = 400;
     sprite.position.y = 300;
 
-    sprite.scale.x = 1;
-    sprite.scale.y = 1;
-
     mainRenderer.loadSprite(sprite);
 
     mainRenderer.render();
