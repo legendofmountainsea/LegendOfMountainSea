b814100d6601b6ec53016201e5358c4352847d85
tyrealgray <tyrealgray@ymail.com>
2017-05-16 23:36:58 +0200

    add control module

--- LOMS/src/main.js
+++ LOMS/src/main.js
@@ -1,10 +1,12 @@
 import LOMSRenderer from './render/lomsRenderer';
+import Controller from './control/controller';
 
-var mainRenderer = new LOMSRenderer;
+var mainRenderer = new LOMSRenderer,
+    controller = new Controller;
 
 mainRenderer.initRenderer();
 
-mainRenderer.addResource('logo','./LOMS.png',(logo)=>{
+mainRenderer.addResource('logo', './LOMS.png', (logo) => {
 
     let sprite = new PIXI.Sprite(logo.texture);
 
@@ -12,7 +14,7 @@ mainRenderer.addResource('logo','./LOMS.png',(logo)=>{
     sprite.position.y = 300;
 
     mainRenderer.loadSprite(sprite);
-    
+
 });
 
 mainRenderer.render();
