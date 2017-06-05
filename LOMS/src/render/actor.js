fa76d6dcc82c5bf6e1f8d21df8e735cf604394d7
tyrealgray <tyrealgray@ymail.com>
2017-06-05 11:59:41 +0200

    change idcounter into size
    Former-commit-id: 3c4bfcc2f94930ae17199949eceb1f553045eb2b

--- LOMS/src/render/actor.js
+++ LOMS/src/render/actor.js
@@ -1,10 +1,26 @@
 import 'pixi.js';
 export default class Actor {
-    constructor(){
+    constructor(props){
+        this._props = props;
 
+        this._init();
     }
 
-    render(){
-        console.log('init actor')
+    _init(){
+        this._sprite = new PIXI.Sprite(this._props.resouce.texture);
+
+            this._sprite.position.x = 400;
+            this._sprite.position.y = 300;
+    }
+
+    setPosition(position){
+        this._sprite.position.x = position.x;
+        this._sprite.position.y = position.y;
+
+        return this;
+    }
+
+    render(delta){
+       
     }
 }
