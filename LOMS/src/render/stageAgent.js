8f656b760df4ad17664d8f55679b21824fbe1ce5
tyrealgray <tyrealgray@ymail.com>
2017-06-05 11:46:37 +0200

    implement class actor
    Former-commit-id: e4e7d7c0adf1b62bf21c3d8d761f19b93e330e9d

--- LOMS/src/render/stageAgent.js
+++ LOMS/src/render/stageAgent.js
@@ -1,15 +1,21 @@
-/**
- * manage all the renderer here
- */
+import Actor from './actor';
 export default class StageAgent {
     constructor(engineProps){
         this._engineProps = engineProps;
-        this._stages = [];
+        this._stages = {};
+        this._idCounter = 0;
+        this._actor = new Actor().render();
     }
 
     addElements(...element) {
-        
+        for(let index = 0; index < element; ++index){
+            this._stages[this._idCounter++] = element[index];
+        }
     };
+
+    removeElements(...element){
+
+    }
 
     //use lomsRenderer render function to call this
     render(delta){
