48b21bf10dfcd068eb6d6efc6ff4892c42f55526
TyrealGray <tyrealgray@ymail.com>
2017-06-07 20:14:22 +0200

    fixed bug: node evn is not fit with nwjs
    Former-commit-id: a336d551b8a09b7d7173505335612d713cbae47a

--- LOMS/webpack.config.js
+++ LOMS/webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 
-module.exports = {
+var clientConfig = {
+	target:'web',
     entry: "./src/main.js",
     output: {
         path: __dirname,
@@ -30,3 +31,37 @@ module.exports = {
         }
     }
 }
+
+var serverConfig = {
+	target:'node',
+    entry: "./src/main.js",
+    output: {
+        path: __dirname,
+        filename: "loms.js"
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            "es2015",
+                            "stage-0"
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'pixi$': path.resolve(__dirname, '/node_modules/pixi.js/bin/pixi.min.js')
+        }
+    }
+}
+
+module.exports = [ serverConfig, clientConfig ];
