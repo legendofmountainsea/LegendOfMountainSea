e18efdcaabc74cd41d112a5faa0e4cd3e82f3446
tyrealgray <tyrealgray@ymail.com>
2017-06-13 10:42:16 +0200

    warp class LOMS
    Former-commit-id: c95455da71fc70133e87d688d5413b0eb6b3baef

--- LOMS/src/main.js
+++ LOMS/src/main.js
@@ -1,24 +1,4 @@
-import LOMSRenderer from './render/lomsRenderer';
-import Controller from './control/controller';
+import LOMS from './app';
 
-import MainMenu from './view/mainMenu';
-
-class LOMSGame {
-    constructor() {
-        this._renderer = new LOMSRenderer;
-        this._controller = new Controller;
-    }
-
-    beginGame() {
-
-        const renderer = this._renderer,
-        controller = this._controller;
-
-        new MainMenu({renderer,controller}).render();
-
-        this._renderer.render();
-    }
-}
-
-const app = new LOMSGame;
+const app = new LOMS;
 app.beginGame();
--- LOMS/src/main.node.js
+++ LOMS/src/main.node.js
@@ -1,28 +1,8 @@
-import LOMSRenderer from './render/lomsRenderer';
-import Controller from './control/controller';
+import LOMS from './app';
 
-import MainMenu from './view/mainMenu';
-
-class LOMSGame {
-    constructor() {
-        this._renderer = new LOMSRenderer;
-        this._controller = new Controller;
-    }
-
-    beginGame() {
-
-        const renderer = this._renderer,
-            controller = this._controller;
-
-        new MainMenu({ renderer, controller }).render();
-
-        this._renderer.render();
-    }
-}
-
-if (typeof window !=="undefined" && root===window) {
+if (typeof window !== "undefined" && root === window) {
     console.log('dev mode');
 } else {
-    const app = new LOMSGame;
+    const app = new LOMS;
     app.beginGame();
 }
