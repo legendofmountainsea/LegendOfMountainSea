e75c025a5aee0c007568abb35a5a251dd7a58228
tyrealgray <tyrealgray@ymail.com>
2017-06-27 22:45:23 +0200

    and isSpriteSheet check for init function
    Former-commit-id: b4ef334a8516801e94a2d8a00be02a63417a4aa8

--- LOMS/src/render/actor.js
+++ LOMS/src/render/actor.js
@@ -3,18 +3,30 @@ export default class Actor {
     constructor(props) {
         this._name = props.name;
         this._path = props.path;
+        this._isSpriteSheet = props.isSpriteSheet? props.isSpriteSheet: false;
         this._initPosition = props.position? props.position : {x:0,y:0};
         this._onRender = props.onRender? props.onRender : null;
         this._sprite = null;
     }
 
     initResouce(resouce){
+
+        if(this._isSpriteSheet){
+            this._frames = [];
+            console.log(resouce);
+            return;
+        }
+
         this._sprite = new PIXI.Sprite(resouce.texture);
 
         this._sprite.anchor.set(0.5,0.5);
 
         this._sprite.position.x = this._initPosition.x;
         this._sprite.position.y = this._initPosition.y;
+    }
+
+    initSpriteSheet(resouce){
+
     }
 
     setID(ID){
