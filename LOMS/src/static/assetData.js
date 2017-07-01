135ed72cb553acfe55a9ca797de1fc9e0d93287b
tyrealgray <tyrealgray@ymail.com>
2017-07-01 23:51:32 +0200

    add houyi character asset date

--- LOMS/src/static/assetData.js
+++ LOMS/src/static/assetData.js
@@ -2,5 +2,9 @@ export default {
     LOGO: {
         NAME: 'logo',
         PATH: './LOMS.png',
+    },
+    HOUYI_STAND:{
+        NAME: 'houyi_stand',
+        PATH: './assets/characters/HouYi/houyi_stand/houyi_stand.json',
     }
 };
