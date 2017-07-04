7f94984797c2f5f53d58f91254abee902ed7acae
tyrealgray <tyrealgray@ymail.com>
2017-07-04 21:09:08 +0200

    change initResources function process
    Former-commit-id: a4a71c9d17118fae25c418389a973d9f01e72f2b

--- LOMS/src/render/character.js
+++ LOMS/src/render/character.js
@@ -2,11 +2,26 @@ import Actor from './actor';
 
 export default class Character extends Actor {
     constructor(props){
-        this.super(props);
+        super(props);
+        this._name = props.name;
+        this._path = props.path;
         this._frames = [];
     }
 
-    initResouce(resouces){
+    initResouces(resouces){
 
+        let resouce = resouces[this.getName()];
+        
+        for(let texture in resouce.textures){
+            this._frames.push(resouce.textures[texture]);
+        }
+        
+        this._sprite = new PIXI.extras.AnimatedSprite(this._frames);
+        this._sprite.anchor.set(0.5, 0.5);
+        this._sprite.animationSpeed = 0.025;
+
+        this._sprite.position.x = this._initPosition.x;
+        this._sprite.position.y = this._initPosition.y;
+        this._sprite.play();
     }
 };
