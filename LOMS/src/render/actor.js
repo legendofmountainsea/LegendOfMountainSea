a4a71c9d17118fae25c418389a973d9f01e72f2b
tyrealgray <tyrealgray@ymail.com>
2017-07-04 21:09:08 +0200

    change initResources function process

--- LOMS/src/render/actor.js
+++ LOMS/src/render/actor.js
@@ -1,45 +1,10 @@
 
 export default class Actor {
     constructor(props) {
-        this._name = props.name;
-        this._path = props.path;
         this._isSpriteSheet = props.isSpriteSheet ? props.isSpriteSheet : false;
         this._initPosition = props.position ? props.position : { x: 0, y: 0 };
         this._onRender = props.onRender ? props.onRender : null;
         this._sprite = null;
-    }
-
-    initResouce(resouce) {
-
-        if (this._isSpriteSheet) {
-            this._frames = [];
-            this.initSpriteSheet(resouce);
-            return;
-        }
-
-        this._sprite = new PIXI.Sprite(resouce.texture);
-
-        this._sprite.anchor.set(0.5, 0.5);
-
-        this._sprite.position.x = this._initPosition.x;
-        this._sprite.position.y = this._initPosition.y;
-    }
-
-    initSpriteSheet(resouce) {
-
-        let frames = [];
-
-        for(let texture in resouce.textures){
-            frames.push(resouce.textures[texture]);
-        }
-        
-        this._sprite = new PIXI.extras.AnimatedSprite(frames);
-        this._sprite.anchor.set(0.5, 0.5);
-        this._sprite.animationSpeed = 0.025;
-
-        this._sprite.position.x = this._initPosition.x;
-        this._sprite.position.y = this._initPosition.y;
-        this._sprite.play();
     }
 
     setID(ID) {
