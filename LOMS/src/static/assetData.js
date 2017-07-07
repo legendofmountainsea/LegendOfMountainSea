eed40d7a39aee1862ced992696c00c11001faddc
tyrealgray <tyrealgray@ymail.com>
2017-07-07 23:03:58 +0200

    change asset json data format #33

--- LOMS/src/static/assetData.js
+++ LOMS/src/static/assetData.js
@@ -4,18 +4,17 @@ export default {
         NAME: 'logo',
         PATH: './LOMS.png',
     },
-    HOUYI_STAND: {
-        IS_CHARACTER: false,
-        NAME: 'houyi_stand',
-        PATH: './assets/characters/HouYi/houyi_stand/houyi_stand.json',
-    },
     HOUYI: {
         IS_CHARACTER: true,
-        HOUYI_WALK: {
+        STAND: {
+            NAME: 'houyi_stand',
+            PATH: './assets/characters/HouYi/houyi_stand/houyi_stand.json',
+        },
+        WALK: {
             NAME: 'houyi_walk',
             PATH: './assets/characters/HouYi/houyi_walk/houyi_walk.json',
         },
-        HOUYI_ATTACK: {
+        ATTACK: {
             NAME: 'houyi_attack',
             PATH: './assets/characters/HouYi/houyi_attack/houyi_attack.json',
         },
