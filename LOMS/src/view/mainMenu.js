5c78e8b1c49b7dd14538897b20748d3b8f981a24
tyrealgray <tyrealgray@ymail.com>
2017-07-07 22:42:33 +0200

    change param in Character constructor
    Former-commit-id: 13277fa4c87203dcbe597d70e27b967e1e8bc0a1

--- LOMS/src/view/mainMenu.js
+++ LOMS/src/view/mainMenu.js
@@ -24,15 +24,13 @@ export default class MainMenu {
             sprite.rotation += (0.01 * delta);
         });
 
-        // let houyi = new Character({
-        //     name: S_assetData.HOUYI_STAND.NAME,
-        //     path: S_assetData.HOUYI_STAND.PATH,
-        //     isSpriteSheet:true,
-        //     position: { x: 300, y: 400 }
-        // });
+        let houyi = new Character({
+            assetData: S_assetData.HOUYI,
+            position: { x: 300, y: 400 }
+        });
 
         this.props.renderer.addActor(logo);
 
-        //this.props.renderer.addActor(houyi);
+        this.props.renderer.addActor(houyi);
     }
 }
