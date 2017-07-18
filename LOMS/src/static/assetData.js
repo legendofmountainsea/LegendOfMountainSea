9be27b3f1124477fabf55a04bd11973a90fb60b7
tyrealgray <tyrealgray@ymail.com>
2017-07-18 21:24:29 +0200

    remove IS_CHARACTER checking in loop all the time
    Former-commit-id: 5c761a6254cbd5625f63a313c28067396ae07815

--- LOMS/src/static/assetData.js
+++ LOMS/src/static/assetData.js
@@ -1,22 +1,26 @@
 export default {
     LOGO: {
         IS_CHARACTER: false,
-        NAME: 'logo',
-        PATH: './LOMS.png',
+        DATA: {
+            NAME: 'logo',
+            PATH: './LOMS.png',
+        },
     },
     HOUYI: {
         IS_CHARACTER: true,
-        STAND: {
-            NAME: 'houyi_stand',
-            PATH: './assets/characters/HouYi/houyi_stand/houyi_stand.json',
-        },
-        WALK: {
-            NAME: 'houyi_walk',
-            PATH: './assets/characters/HouYi/houyi_walk/houyi_walk.json',
-        },
-        ATTACK: {
-            NAME: 'houyi_attack',
-            PATH: './assets/characters/HouYi/houyi_attack/houyi_attack.json',
+        DATA: {
+            STAND: {
+                NAME: 'houyi_stand',
+                PATH: './assets/characters/HouYi/houyi_stand/houyi_stand.json',
+            },
+            WALK: {
+                NAME: 'houyi_walk',
+                PATH: './assets/characters/HouYi/houyi_walk/houyi_walk.json',
+            },
+            ATTACK: {
+                NAME: 'houyi_attack',
+                PATH: './assets/characters/HouYi/houyi_attack/houyi_attack.json',
+            },
         },
     }
 };
