97eb524d81810f236510e3e181670f890c906344
tyrealgray <tyrealgray@ymail.com>
2017-07-23 11:45:03 +0200

    add mouse down api for pawn and controller
    Former-commit-id: 74109d332c58bf7fa801589d43f4903e0687e74d

--- LOMS/src/render/stageAgent.js
+++ LOMS/src/render/stageAgent.js
@@ -13,7 +13,7 @@ export default class StageAgent {
         this._controller = new Controller();
         this._engineProps.stage.hitArea = new PIXI.Rectangle(0, 0, 990, 768);
         this._engineProps.stage.mousedown = (e) => {
-            console.log(e)
+            console.log(e.data.originalEvent.layerX, e.data.originalEvent.layerY)
         };
     }
 
