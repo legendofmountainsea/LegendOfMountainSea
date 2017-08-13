6db42ed2403be38f4ef5affa6ba0002dfcce35a6
tyrealgray <tyrealgray@ymail.com>
2017-08-13 13:46:24 +0200

    add title style for uiText

--- LOMS/src/render/uiText.js
+++ LOMS/src/render/uiText.js
@@ -4,6 +4,41 @@ export default class UIText {
         this._style = props.style;
         this._initPosition = props.position? props.position : {x:0,y:0};
         this._onRender = props.onRender? props.onRender : null;
+        this._onClick = props.onClick? props.onClick : null;
         this._text = null;
+        this._sprite = null;
+    }
+
+    getSprite() {
+        return this._sprite;
+    }
+
+    setPosition(position) {
+        if (this._sprite) {
+            this._sprite.position.x = position.x;
+            this._sprite.position.y = position.y;
+        }
+
+        return this;
+    }
+
+    bindRender(onRender) {
+        this._onRender = onRender;
+        return this;
+    }
+
+    render(delta) {
+        this.tick(delta);
+        this.onRender(delta);
+    }
+
+    onRender(delta) {
+        if (this._onRender && this._sprite) {
+            this._onRender(this._sprite, delta);
+        }
+    }
+
+    tick(delta){
+        //override in subClass
     }
 }
