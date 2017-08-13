5dfc76775e6bbe76a736d8e3f093cd411ed0e57f
tyrealgray <tyrealgray@ymail.com>
2017-08-13 13:46:24 +0200

    add title style for uiText
    Former-commit-id: 6db42ed2403be38f4ef5affa6ba0002dfcce35a6

---
+++ LOMS/src/static/testStyle.js
@@ -0,0 +1,18 @@
+export default {
+    TITLE: {
+        fontFamily: 'Arial',
+        fontSize: 18,
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+        fill: ['#ffffff'],
+        stroke: '#4a1850',
+        strokeThickness: 5,
+        dropShadow: true,
+        dropShadowColor: '#000000',
+        dropShadowBlur: 4,
+        dropShadowAngle: Math.PI / 6,
+        dropShadowDistance: 6,
+        wordWrap: true,
+        wordWrapWidth: 440
+    }
+}
