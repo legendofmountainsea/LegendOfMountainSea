e0332d613a8eb305b0ddfa41d3618b4ebe9b59d1
tyrealgray <tyrealgray@ymail.com>
2017-09-23 14:26:03 +0200

    use scale x to manage animation direction
    Former-commit-id: 7b58181029dd81071a398939e1cfb3f7f5170b8e

--- LOMS/src/static/worldAsset.js
+++ LOMS/src/static/worldAsset.js
@@ -2,43 +2,23 @@ export default {
     HOUYI: {
         IS_CHARACTER: true,
         DATA: {
-            STAND0: {
-                NAME: 'houyi_stand0',
+            STAND: {
+                NAME: 'houyi_stand',
                 SPEED: 0.025,
                 PATH: './assets/characters/HouYi/houyi_stand/houyi_stand.json',
             },
-            WALK0: {
-                NAME: 'houyi_walk0',
+            WALK: {
+                NAME: 'houyi_walk',
                 SPEED: 0.08,
                 PATH: './assets/characters/HouYi/houyi_walk/houyi_walk.json',
             },
-            ATTACK0: {
-                NAME: 'houyi_attack0',
+            ATTACK: {
+                NAME: 'houyi_attack',
                 SPEED: 0.25,
                 PATH: './assets/characters/HouYi/houyi_attack/houyi_attack.json',
             },
-            BATTLE0: {
-                NAME: 'houyi_battle0',
-                SPEED: 0.025,
-                PATH: './assets/characters/HouYi/houyi_battle/houyi_battle.json',
-            },
-            STAND1: {
-                NAME: 'houyi_stand1',
-                SPEED: 0.025,
-                PATH: './assets/characters/HouYi/houyi_stand/houyi_stand.json',
-            },
-            WALK1: {
-                NAME: 'houyi_walk1',
-                SPEED: 0.08,
-                PATH: './assets/characters/HouYi/houyi_walk/houyi_walk.json',
-            },
-            ATTACK1: {
-                NAME: 'houyi_attack1',
-                SPEED: 0.25,
-                PATH: './assets/characters/HouYi/houyi_attack/houyi_attack.json',
-            },
-            BATTLE1: {
-                NAME: 'houyi_battle1',
+            BATTLE: {
+                NAME: 'houyi_battle',
                 SPEED: 0.025,
                 PATH: './assets/characters/HouYi/houyi_battle/houyi_battle.json',
             },
