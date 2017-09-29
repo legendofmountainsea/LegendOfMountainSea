b68d44bacb6c595001cf28416c9761617df72e5d
tyrealgray <tyrealgray@ymail.com>
2017-09-29 22:37:57 +0200

    add jasmine for unit test, try to fix error in CI
    Former-commit-id: 5d764ddef12a1d9465c3bb59fb63129a196231d6

---
+++ LOMS/spec/controller/PawnSpec.js
@@ -0,0 +1,20 @@
+import Pawn from '../../src/control/pawn';
+import Character from '../../src/render/character';
+
+describe("Pawn", function () {
+    let pawn;
+
+    beforeEach(function () {
+        pawn = new Pawn({character: new Character({noAsset: true, position: {x: 0, y: 0}})});
+    });
+
+    it("should be able to set destination", function () {
+        const mouseEvent = {data: {originalEvent: {layerX: 5, layerY: 6}}};
+        pawn.onMouseDown(mouseEvent);
+
+        const character = pawn.getCharacter();
+        expect(character.getDestination().x).toBe(5);
+        expect(character.getDestination().y).toBe(6);
+    });
+
+});
