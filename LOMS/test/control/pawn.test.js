100664a16c54ef50eb0a07439c9c4569603413f6
tyrealgray <tyrealgray@ymail.com>
2017-10-01 15:48:40 +0200

    create unit test for character class refactor character.js to make code more readable

---
+++ LOMS/test/control/pawn.test.js
@@ -0,0 +1,34 @@
+import expect from 'expect.js';
+import Pawn from '../../src/control/pawn';
+import Character from '../../src/render/character';
+
+describe("Pawn", () => {
+    let pawn;
+
+    beforeEach(() => {
+        pawn = new Pawn({
+            character: new Character({
+                noAsset: true,
+                position: {x: 0, y: 0}
+            })
+        });
+    });
+
+    afterEach(() => {
+        pawn = null;
+    });
+
+    it("should be able to set destination", () => {
+        const mouseEvent = {
+            data: {
+                originalEvent: {layerX: 5, layerY: 6}
+            }
+        };
+        pawn.onMouseDown(mouseEvent);
+
+        const character = pawn.getCharacter();
+        expect(character.getDestination().x).to.be(5);
+        expect(character.getDestination().y).to.be(6);
+    });
+
+});
