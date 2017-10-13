37fbf825b49c6748c2802ba0f82f7e93b1a21a81
tyrealgray <tyrealgray@ymail.com>
2017-10-14 00:08:47 +0200

    mouse component will move with mousemove event #46

--- LOMS/src/render/mouse.js
+++ LOMS/src/render/mouse.js
@@ -2,6 +2,7 @@ const MOUSE_TEXTURE_PATH = '../../assets/mouse.png';
 
 export default class Mouse {
     constructor(){
+        this._isOut = false;
         this._sprite = null;
     }
 
@@ -12,6 +13,24 @@ export default class Mouse {
 
     getSprite(){
         return this._sprite;
+    }
+
+    setPosition(position) {
+        if (this._sprite) {
+            this._sprite.position.x = position.x;
+            this._sprite.position.y = position.y;
+        }
+
+        return this;
+    }
+
+    setOut(isMouseOut){
+        this._isOut = isMouseOut;
+        return this;
+    }
+
+    isOut(){
+        return this._isOut;
     }
 
     tick(delta){
