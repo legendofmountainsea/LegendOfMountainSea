c558a4878f23cdc5e251f50812533fae39b96aea
tyrealgray <tyrealgray@ymail.com>
2017-10-16 00:02:07 +0200

    bugfix #31

--- LOMS/gui.js
+++ LOMS/gui.js
@@ -1,9 +1,7 @@
 //set NW.js client window size
-//TODO https://github.com/SkyHarp/LegendOfMountainSea/issues/31
 var wingui = require('nw.gui').Window.get();
 
 if (wingui !== 'undefined') {
-    wingui.setMinimumSize(1024, 768);
+    wingui.maximize();
     wingui.setResizable(false);
-    wingui.moveTo(0, 0);
 }
