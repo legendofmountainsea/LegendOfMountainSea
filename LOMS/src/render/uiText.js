0e4b0bd7497e6d7e923d0609c5b14d7ef3bf2ad5
rrandom <emanonhere@gmail.com>
2017-10-17 23:49:39 +0800

    spread operator instead of object.assign

--- LOMS/src/render/uiText.js
+++ LOMS/src/render/uiText.js
@@ -18,10 +18,11 @@ export default class UIText extends Actor {
                 this._onClick(e);
             }
             this._sprite.mouseover = (e) => {
-                this._sprite.style = Object.assign({}, this._style, {
-                    stroke: 'white',
-                    strokeThickness: 5,
-                });
+                this._sprite.style = { ...this._style, ...{
+                        stroke: 'white',
+                        strokeThickness: 5,
+                    }
+                };
             }
             this._sprite.mouseout = (e) => {
                 this._sprite.style = this._style;
