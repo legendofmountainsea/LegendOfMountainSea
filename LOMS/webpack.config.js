7cb414ea23c3eaaf7aedd7531e68f8ba8484b2c4
tyrealgray <tyrealgray@ymail.com>
2017-10-30 19:50:29 +0100

    remove comments while uglify js code

--- LOMS/webpack.config.js
+++ LOMS/webpack.config.js
@@ -1,72 +1,78 @@
 var path = require('path'),
-    webpack = require('webpack');
+	webpack = require('webpack');
 
 var clientConfig = {
-    target: 'web',
-    entry: './src/main.js',
-    output: {
-        path: __dirname,
-        filename: 'loms.js',
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env'],
-                        plugins: [require('babel-plugin-transform-object-rest-spread'), require('babel-plugin-transform-class-properties')],
-                    },
-                },
-            },
-        ],
-    },
-    resolve: {
-        alias: {
-            'pixi': path.resolve(__dirname, '/node_modules/pixi.js/bin/pixi.min.js'),
-        },
-    },
-    plugins: [
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        new webpack.optimize.UglifyJsPlugin(),
-    ],
+	target: 'web',
+	entry: './src/main.js',
+	output: {
+		path: __dirname,
+		filename: 'loms.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env'],
+						plugins: [require('babel-plugin-transform-object-rest-spread'), require('babel-plugin-transform-class-properties')],
+					},
+				},
+			},
+		],
+	},
+	resolve: {
+		alias: {
+			'pixi': path.resolve(__dirname, '/node_modules/pixi.js/bin/pixi.min.js'),
+		},
+	},
+	plugins: [
+		new webpack.optimize.ModuleConcatenationPlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			output: {
+				comments: false,
+			},
+		}),
+	],
 };
 
 var nodeConfig = {
-    target: 'node',
-    entry: './src/main.node.js',
-    output: {
-        path: __dirname,
-        filename: 'loms.node.js',
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env'],
-                        plugins: [require('babel-plugin-transform-object-rest-spread'), require('babel-plugin-transform-class-properties')],
-                    },
-                },
-            },
-        ],
-    },
-    resolve: {
-        alias: {
-            'pixi': path.resolve(__dirname, '/node_modules/pixi.js/bin/pixi.min.js'),
-        },
-    },
-    plugins: [
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        new webpack.optimize.UglifyJsPlugin(),
-    ],
+	target: 'node',
+	entry: './src/main.node.js',
+	output: {
+		path: __dirname,
+		filename: 'loms.node.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env'],
+						plugins: [require('babel-plugin-transform-object-rest-spread'), require('babel-plugin-transform-class-properties')],
+					},
+				},
+			},
+		],
+	},
+	resolve: {
+		alias: {
+			'pixi': path.resolve(__dirname, '/node_modules/pixi.js/bin/pixi.min.js'),
+		},
+	},
+	plugins: [
+		new webpack.optimize.ModuleConcatenationPlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			output: {
+				comments: false,
+			},
+		}),
+	],
 };
 
 module.exports = [nodeConfig, clientConfig];
