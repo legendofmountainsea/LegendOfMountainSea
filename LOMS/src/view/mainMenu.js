c87ee23ab718ac776deaecc61f81e2b2092f44b0
tyrealgray <tyrealgray@ymail.com>
2017-11-01 22:03:47 +0100

    Add Quit Button in Main Menu #55
    Former-commit-id: 134ade953c5c7a4ecb9277eb25e89ca978dee9ca

--- LOMS/src/view/mainMenu.js
+++ LOMS/src/view/mainMenu.js
@@ -8,57 +8,68 @@ import Style from '../static/textStyle';
 import WorldScene from '../scene/worldScene';
 
 export default class MainMenu extends Scene {
-    constructor(props) {
-        super(props);
-        this._assetsData = [S_mainMenuAsset];
-        this._onFinish = this.onFinish.bind(this);
-        this._backGroundMusic = null;
-    }
-
-    onFinish() {
-        this._backGroundMusic  = new Audio('../../assets/sound/Angelic_Happiness.mp3');
-        this._backGroundMusic.loop = true;
-        this._backGroundMusic.play();
-
-        let logo = new Pattern({
-            assetData: S_mainMenuAsset.LOGO,
-            position: { x: 200, y: 200 },
-        });
-
-        logo.bindRender((sprite, delta) => {
-            sprite.rotation += (0.01 * delta);
-        });
-
-        let newGameText = new UIText({
-            string: 'New Game',
-            position: { x: 300, y: 400 },
-            style: Style.MAIN_MENU,
-            onClick: (e) => {
-                this.dispose();
-                const worldScene = new WorldScene;
-                this._renderer.renderScene(worldScene);
-                this._backGroundMusic.pause();
-            },
-        });
-
-        let loadGameText = new UIText({
-            string:'Load Game',
-            position: { x: 300, y: 450 },
-            style: Style.MAIN_MENU,
-            onClick: (e) => {
-                //TODO https://github.com/SkyHarp/LegendOfMountainSea/issues/26
-            },
-        });
-
-        this._renderer.addActor(newGameText);
-        this._renderer.addActor(loadGameText);
-        this._renderer.addActor(logo);
-
-        return this;
-    }
-
-    dispose(){
-        this._renderer.clearStage();
-        return this;
-    }
+	constructor(props) {
+		super(props);
+		this._assetsData = [S_mainMenuAsset];
+		this._onFinish = this.onFinish.bind(this);
+		this._backGroundMusic = null;
+	}
+	
+	onFinish() {
+		this._backGroundMusic = new Audio('../../assets/sound/Angelic_Happiness.mp3');
+		this._backGroundMusic.loop = true;
+		this._backGroundMusic.play();
+		
+		let logo = new Pattern({
+			assetData: S_mainMenuAsset.LOGO,
+			position: {x: 200, y: 200},
+		});
+		
+		logo.bindRender((sprite, delta) => {
+			sprite.rotation += (0.01 * delta);
+		});
+		
+		let newGameText = new UIText({
+			string: 'New Game',
+			position: {x: 400, y: 400},
+			style: Style.MAIN_MENU,
+			onClick: (e) => {
+				this.dispose();
+				const worldScene = new WorldScene;
+				this._renderer.renderScene(worldScene);
+				this._backGroundMusic.pause();
+			},
+		});
+		
+		let loadGameText = new UIText({
+			string: 'Load Game',
+			position: {x: 400, y: 450},
+			style: Style.MAIN_MENU,
+			onClick: (e) => {
+				//TODO https://github.com/SkyHarp/LegendOfMountainSea/issues/26
+			},
+		});
+		
+		let quitGameText = new UIText({
+			string: 'Quit',
+			position: {x: 400, y: 500},
+			style: Style.MAIN_MENU,
+			onClick: (e) => {
+				winGUI ? winGUI.close() : window.close();
+			},
+		});
+		
+		this._renderer.addActor(logo);
+		
+		this._renderer.addActor(newGameText);
+		this._renderer.addActor(loadGameText);
+		this._renderer.addActor(quitGameText);
+		
+		return this;
+	}
+	
+	dispose() {
+		this._renderer.clearStage();
+		return this;
+	}
 }
