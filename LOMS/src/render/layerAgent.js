b1fa8c26a1b88a0dbc98d601011a004dc8ba16a2
tyrealgray <tyrealgray@ymail.com>
2017-11-02 23:23:02 +0100

    #49 implementing layerAgent's query element feature
    Former-commit-id: 48eb99cbb6432493be368945ef75225769430170

--- LOMS/src/render/layerAgent.js
+++ LOMS/src/render/layerAgent.js
@@ -1,4 +1,55 @@
 export default class LayerAgent {
-	constructor(props){
-	}
+    constructor(props) {
+        this._stage = props.stage;
+        this._layer = {};
+        this._elements = {};
+        this._size = 0;
+    }
+
+    init() {
+
+    }
+
+    addElement(element, index = 0) {
+        let ID = ++this._size;
+
+        element.setID(ID);
+
+        this._elements[ID] = element;
+
+        this._addToLayer(element, index);
+
+        const result = this._getTopElementInLayer(index);
+
+        const childIndex = this._stage.getChildIndex(result);
+
+        this._stage.addChildAt(this._elements[ID].getSprite(), childIndex);
+
+        return this;
+    }
+
+    _addToLayer(element, index) {
+        if (!this._layer[index]) {
+            this._layer[index] = [];
+        }
+
+        this._layer[index].push(element);
+    }
+
+    _getTopElementInLayer(index) {
+
+        if(0 === index){
+            return index;
+        }
+
+        if(!this._layer[index]){
+            return this._getTopElementInLayer(index - 1);
+        }
+        return this._layer[index][0];
+    }
+
+
+    render(tick) {
+
+    }
 }
