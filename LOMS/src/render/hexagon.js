44335c05f6eb35f100c3a82d43282d2672e1e6da
tyrealgray <tyrealgray@ymail.com>
2017-12-07 00:41:49 +0100

    fixed bug terrain position x with negative number caused hexagon position goes wrong
    Former-commit-id: c96eca5869d41efe175f61197146f26fa25e0bdc

--- LOMS/src/render/hexagon.js
+++ LOMS/src/render/hexagon.js
@@ -38,7 +38,7 @@ export default class Hexagon extends Actor {
     setPosition(position) {
         if (this._sprite) {
             this._sprite.position.x = (this._width / 2) + position.x * (this._height * COS_60_DEGREES );
-            this._sprite.position.y = position.y * this._height + (this._height / 2) * (1 + position.x % 2);
+            this._sprite.position.y = position.y * this._height + (this._height / 2) * (1 + Math.abs(position.x) % 2);
         }
 
         return this;
