2429b106d25b7a6f1924a21d43d1bc21765883b3
tyrealgray <tyrealgray@ymail.com>
2017-12-31 00:02:39 +0100

    automatically load language info from native
    Former-commit-id: de278c1ebf5a9c38bb4608c59a2f927204651819

--- LOMS/src/app.js
+++ LOMS/src/app.js
@@ -4,15 +4,15 @@ import LOMSRenderer from './render/lomsRenderer';
 import MainMenu from './view/mainMenu';
 
 export default class LOMS {
-    constructor() {
-        this._renderer = null;
-    }
-
-    beginGame() {
-        setTimeout(()=>{
-            this._renderer = new LOMSRenderer;
-            this._renderer.renderScene(new MainMenu);
-            this._renderer.render();
-        },1000);
-    }
+	constructor() {
+		this._renderer = null;
+	}
+	
+	beginGame() {
+		setTimeout(() => {
+			this._renderer = new LOMSRenderer;
+			this._renderer.renderScene(new MainMenu);
+			this._renderer.render();
+		}, 1000);
+	}
 }
