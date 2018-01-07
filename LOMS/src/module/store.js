6fd8dd851cfc0920651d1742e2315d4047aa4c43
tyrealgray <tyrealgray@ymail.com>
2018-01-07 22:02:01 +0100

    define store api #26

--- LOMS/src/module/store.js
+++ LOMS/src/module/store.js
@@ -1,4 +1,26 @@
 //TODO https://github.com/SkyHarp/LegendOfMountainSea/issues/26
 export default class Store {
-
+	constructor(){
+		this._fileSystem = fileSystem ? fileSystem : null;
+	}
+	
+	clearConfig(){
+	
+	}
+	
+	readConfig(){
+	
+	}
+	
+	writeConfig(){
+	
+	}
+	
+	addConfig(){
+	
+	}
+	
+	removeConfig(){
+	
+	}
 }
