6d59da849c4bc9ce4caa08b12519c9820936e428
tyrealgray <tyrealgray@ymail.com>
2018-01-11 23:49:14 +0100

    fix issue with start nw client in window

--- LOMS/src/util/envUtil.js
+++ LOMS/src/util/envUtil.js
@@ -1,7 +1,7 @@
 import {NativeModuleMissError} from './errorUtil';
 
 export function EXECUTE_IN_CLIENT(method) {
-	if (typeof window !== 'undefined') {
+	if (typeof window !== 'undefined' && typeof nw === 'undefined') {
 		console.warn('Some feature is not showing in web mode.');
 	} else {
 		method();
@@ -9,7 +9,7 @@ export function EXECUTE_IN_CLIENT(method) {
 }
 
 export function EXECUTE_IN_CLIENT_WITH_RETURN(method) {
-	if (typeof window !== 'undefined') {
+	if (typeof window !== 'undefined' && typeof nw === 'undefined') {
 		throw new NativeModuleMissError();
 	} else {
 		return method();
