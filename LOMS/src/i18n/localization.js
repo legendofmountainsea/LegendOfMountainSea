7bf49210730b8c6d70ac213e11ea2bf1e708477b
tyrealgray <tyrealgray@ymail.com>
2018-02-08 23:16:57 +0100

    add i18n api #67
    Former-commit-id: aeb4869a25aef135c17692d73bfc0237b4e379ad

--- LOMS/src/i18n/localization.js
+++ LOMS/src/i18n/localization.js
@@ -2,8 +2,19 @@
 
 import Store from '../module/store';
 
+let content = null;
 export default class Localization {
 	static getCurrentLanguage(){
 		return Store.getConfig().language;
+	}
+	
+	static getLanguageContent(){
+		if(!content){
+		
+		}
+	}
+	
+	static initContent(){
+		
 	}
 }
