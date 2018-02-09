c54a4615cb42f3b198ab0ad6b5ad69e874c972c9
tyrealgray <tyrealgray@ymail.com>
2018-02-09 20:55:55 +0100

    dynamically loading language content #67

--- LOMS/src/i18n/localization.js
+++ LOMS/src/i18n/localization.js
@@ -9,12 +9,30 @@ export default class Localization {
 	}
 	
 	static getLanguageContent(){
+		alert(content);
 		if(!content){
-		
+			content = Localization._getInitContent();
 		}
+		
+		return content;
 	}
 	
-	static initContent(){
+	static _getInitContent(){
+		const supportLanguages = Localization.getSupportLanguages();
 		
+		let language = supportLanguages[Localization.getCurrentLanguage()];
+		
+		if(!language){
+			language = supportLanguages['zh-CN'];
+		}
+		
+		return language.content;
+	}
+	
+	static getSupportLanguages(){
+		return {
+			'zh-CN': require('./zh-CN/zh-CN'),
+			'en-US': require('./en-US/en-US'),
+		};
 	}
 }
