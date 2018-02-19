72a6c25cc7f2de4cd912d6cd3456237583fb72e2
tyrealgray <tyrealgray@ymail.com>
2018-02-19 15:02:33 +0100

    using jQuery and bootstrap for game GUI #72

--- LOMS/src/view/mainMenu.js
+++ LOMS/src/view/mainMenu.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 import S_mainMenuAsset from '../static/mainMenuAsset';
 
 import Scene from '../scene/scene';
@@ -54,6 +55,28 @@ export default class MainMenu extends Scene {
 			position: {x: 400, y: 450},
 			style: Style.MAIN_MENU,
 			onClick: (e) => {
+				const modalTemplate = `<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>`;
+				$('#GUIContainer').html(modalTemplate);
+				
+				$('#exampleModalCenter').modal();
 				//TODO https://github.com/SkyHarp/LegendOfMountainSea/issues/26
 			},
 		});
