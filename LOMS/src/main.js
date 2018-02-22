78280a012833b066b461ed797c76e62e73b505cd
Jimkwan <jimkwan.yau@gmail.com>
2018-02-22 15:20:37 +0800

    add a on screen console panel for debug #71
    Former-commit-id: 77c6d5d4b177e298c89edd45b6c471fb376089eb

--- LOMS/src/main.js
+++ LOMS/src/main.js
@@ -1,7 +1,10 @@
 import LOMS from './app';
 import {EXECUTE_WITHOUT_CLIENT} from './util/envUtil';
+// import {oConsole} from './util/errorUtil';
 
 EXECUTE_WITHOUT_CLIENT(()=>{
 	const app = new LOMS;
 	app.beginGame();
+	// oConsole.enable();
+	// oConsole.show();
 });
