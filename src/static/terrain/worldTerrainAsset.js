3b3ed03f3ff8d9d3e78a2b7bbd89920297dc8068
tyrealgray <tyrealgray@ymail.com>
2018-03-08 23:25:05 +0100

    have fun with new texture for terrain #69
    Former-commit-id: 1d117636efda0c5303b26b1069021a7b185f72b3

--- src/static/terrain/worldTerrainAsset.js
+++ src/static/terrain/worldTerrainAsset.js
@@ -6,4 +6,18 @@ export default {
             PATH: './assets/hexagon.png',
         },
     },
+    FOREST: {
+        IS_CONTAIN_ANIMATION: false,
+        DATA: {
+            NAME: 'forest',
+            PATH: './assets/terrains/forest.png',
+        },
+    },
+	HILL: {
+		IS_CONTAIN_ANIMATION: false,
+		DATA: {
+			NAME: 'hill',
+			PATH: './assets/terrains/hills.png',
+		},
+	},
 };
