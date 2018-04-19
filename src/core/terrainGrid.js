cdd0e21095205e3c01ed943b7d890a4a84262f10
tyrealgray <tyrealgray@ymail.com>
2018-04-19 21:09:59 +0200

    implement getAssetData function

--- src/core/terrainGrid.js
+++ src/core/terrainGrid.js
@@ -1,36 +1,48 @@
 import Grid from './grid';
 import Coordinates from './coordinates';
+import S_worldTerrainAsset, {TERRAIN_ID, FOREST_ID, HILL_ID} from '../static/terrain/worldTerrainAsset';
+
+export const ASSETS_NUMBER = 3;
 
 export default class TerrainGrid extends Grid {
-    constructor(props){
-        super(props);
-    }
-
-    getData(center, range){
-
-        const length = range.radius * 2 + 1,
-            coordinatesSet = [];
-
-        for ( let index = 0; index < length; ++index){
-            let blockRow = [];
-            for (let columnIndex = 0; columnIndex < length; ++columnIndex){
-                let coordinatesX = center.x - (range.radius - 1) - Math.floor(index/2) + columnIndex,
-                    coordinatesY = center.y - range.radius + index;
-                blockRow.push(new Coordinates(coordinatesX,coordinatesY));
-            }
-            coordinatesSet.push(blockRow);
-        }
-
-        return coordinatesSet;
-    }
-
-    getAssetData(id){
-        let asset = null;
-        switch (id){
-            default:
-                break;
-        }
-
-        return asset;
-    }
+	constructor(props) {
+		super(props);
+	}
+	
+	getData(center, range) {
+		
+		const length = range.radius * 2 + 1,
+			coordinatesSet = [];
+		
+		for (let index = 0; index < length; ++index) {
+			let blockRow = [];
+			for (let columnIndex = 0; columnIndex < length; ++columnIndex) {
+				let coordinatesX = center.x - (range.radius - 1) - Math.floor(index / 2) + columnIndex,
+					coordinatesY = center.y - range.radius + index;
+				blockRow.push(new Coordinates(coordinatesX, coordinatesY));
+			}
+			coordinatesSet.push(blockRow);
+		}
+		
+		return coordinatesSet;
+	}
+	
+	static getAssetData(id) {
+		let asset = null;
+		switch (id) {
+			case TERRAIN_ID:
+				asset = S_worldTerrainAsset.TERRAIN;
+				break;
+			case FOREST_ID:
+				asset = S_worldTerrainAsset.FOREST;
+				break;
+			case HILL_ID:
+				asset = S_worldTerrainAsset.HILL;
+				break;
+			default:
+				break;
+		}
+		
+		return asset;
+	}
 }
--- src/static/terrain/worldTerrainAsset.js
+++ src/static/terrain/worldTerrainAsset.js
@@ -1,18 +1,22 @@
+export const TERRAIN_ID = 0;
+export const FOREST_ID = 1;
+export const HILL_ID = 2;
+
 export default {
-    TERRAIN: {
-        IS_CONTAIN_ANIMATION: false,
-        DATA: {
-            NAME: 'terrain',
-            PATH: './assets/hexagon.png',
-        },
-    },
-    FOREST: {
-        IS_CONTAIN_ANIMATION: false,
-        DATA: {
-            NAME: 'forest',
-            PATH: './assets/terrains/forest.png',
-        },
-    },
+	TERRAIN: {
+		IS_CONTAIN_ANIMATION: false,
+		DATA: {
+			NAME: 'terrain',
+			PATH: './assets/hexagon.png',
+		},
+	},
+	FOREST: {
+		IS_CONTAIN_ANIMATION: false,
+		DATA: {
+			NAME: 'forest',
+			PATH: './assets/terrains/forest.png',
+		},
+	},
 	HILL: {
 		IS_CONTAIN_ANIMATION: false,
 		DATA: {
