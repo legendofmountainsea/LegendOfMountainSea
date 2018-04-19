a46fd8d53d903acaf08bdec0d7e85a568f3400a7
tyrealgray <tyrealgray@ymail.com>
2018-04-19 22:02:36 +0200

    add unit test for randomSeed class

---
+++ test/core/randomSeed.test.js
@@ -0,0 +1,38 @@
+import expect from 'expect.js';
+import RandomSeed from '../../src/core/randomSeed';
+
+describe('RandomSeed', () => {
+	let randomSeed;
+	
+	beforeEach(() => {
+		randomSeed = new RandomSeed({
+			seed:1126,
+		});
+	});
+	
+	afterEach(() => {
+		randomSeed = null;
+	});
+	
+	it('seed 1126 first random number should always be 0.10555126886145405', () => {
+		expect(randomSeed.random()).to.be(0.10555126886145405);
+		randomSeed.setSeed(1126);
+		expect(randomSeed.random()).to.be(0.10555126886145405);
+	});
+	
+	it('seed 1126 second random number should always be 0.9436728395061729', () => {
+		randomSeed.random();
+		expect(randomSeed.random()).to.be(0.9436728395061729);
+		randomSeed.setSeed(1126);
+		randomSeed.random();
+		expect(randomSeed.random()).to.be(0.9436728395061729);
+	});
+	
+	it('seed 2 first random number should not be 0.10555126886145405', () => {
+		randomSeed.setSeed(2);
+		expect(randomSeed.random()).to.not.be(0.10555126886145405);
+		randomSeed.setSeed(2);
+		expect(randomSeed.random()).to.not.be(0.10555126886145405);
+	});
+	
+});
