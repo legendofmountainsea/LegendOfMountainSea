2b65ef020911c88271eda32e0540b430a45a6719
tyrealgray <tyrealgray@ymail.com>
2018-04-25 21:49:28 +0200

    finish Perlin noise terrain creation feature #40 change TerrainChain api
    Former-commit-id: 68494862deba26f0a482c276c5fc8fd1425dc2a7

--- src/chain/terrainChain.js
+++ src/chain/terrainChain.js
@@ -1,43 +1,43 @@
 import Perlin from 'loms.perlin';
 import Coordinates from '../core/coordinates';
 import RandomSeed from '../core/randomSeed';
+import S_worldTerrainAsset, {ASSETS_ID} from '../static/terrain/worldTerrainAsset';
 
-let terrainChainValue = {x:0,y:0};
+export const ASSETS_NUMBER = 4;
 
 let renderCenter = null;
 let seed = null;
 export default class TerrainChain {
-	static getValue(){
-		return terrainChainValue;
+	static _initRenderCenter(){
+		renderCenter = new Coordinates(0,0);
+
+		return this;
 	}
 	
-	static setValue(value){
-		terrainChainValue.x += value.x;
-		terrainChainValue.y += value.y;
-	}
-	
-	static setCenterCoordinates(coordinates){
-		if(!renderCenter){
-			renderCenter = new Coordinates(coordinates.x,coordinates.y);
+	static updateCenterCoordinates(coordinates) {
+		if (!renderCenter) {
+			TerrainChain._initRenderCenter();
 		}
-		else {
-			renderCenter.set(coordinates);
-		}
+		
+		renderCenter.set({
+			x: renderCenter.x + coordinates.x,
+			y: renderCenter.y + coordinates.y,
+		});
 		
 		return this;
 	}
 	
-	static getCenterCoordinates(){
-		if(!renderCenter){
-			renderCenter = new Coordinates(0,0);
+	static getCenterCoordinates() {
+		if (!renderCenter) {
+			TerrainChain._initRenderCenter();
 		}
 		
 		return renderCenter;
 	}
 	
 	// TODO: init seed in other place
-	static getTerrainAssetData(renderPoint){
-		if(!seed){
+	static getTerrainAssetData(renderPoint) {
+		if (!seed) {
 			seed = new RandomSeed();
 		}
 		Perlin.seed(seed.random());
@@ -46,6 +46,46 @@ export default class TerrainChain {
 			vy = renderPoint.y * 0.2;
 		
 		
-		let height = Perlin.perlin2(vx, vy);
+		let noise = Perlin.perlin2(vx, vy);
+		let height = parseInt((noise * 255)) + 135;
+		
+		return TerrainChain.getAssetData(TerrainChain.getTerrainType(height));
+	}
+	
+	static getTerrainType(height) {
+		let type = null;
+		if (height > 200) {
+			type = ASSETS_ID.HILL_ID;
+		} else if (height > 150) {
+			type = ASSETS_ID.FOREST_ID;
+		} else if (height > 100) {
+			type = ASSETS_ID.RIVER_ID;
+		} else {
+			type = ASSETS_ID.SWAMP_ID;
+		}
+		
+		return type;
+	}
+	
+	static getAssetData(id) {
+		let asset = null;
+		switch (id) {
+			case ASSETS_ID.FOREST_ID:
+				asset = S_worldTerrainAsset.FOREST;
+				break;
+			case ASSETS_ID.HILL_ID:
+				asset = S_worldTerrainAsset.HILL;
+				break;
+			case ASSETS_ID.RIVER_ID:
+				asset = S_worldTerrainAsset.RIVER;
+				break;
+			case ASSETS_ID.SWAMP_ID:
+				asset = S_worldTerrainAsset.SWAMP;
+				break;
+			default:
+				break;
+		}
+		
+		return asset;
 	}
 }
