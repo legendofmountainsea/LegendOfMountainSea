f71d38918b2945409cf1069b49efb055870c4a06
tyrealgray <tyrealgray@ymail.com>
2018-04-25 21:53:52 +0200

    fix eslint issue

--- src/core/coordinates.js
+++ src/core/coordinates.js
@@ -1,11 +1,11 @@
 export default class Coordinates {
-    constructor(x,y){
-        this.x = x;
-        this.y = y;
-    }
-    
-    set(coordinates){
-	    this.x = coordinates.x;
-	    this.y = coordinates.y;
-    }
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	set(coordinates) {
+		this.x = coordinates.x;
+		this.y = coordinates.y;
+	}
 }
