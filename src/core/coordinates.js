fc3dccb984a3c3c6109b9ed3dfb7adb230df4ad1
tyrealgray <tyrealgray@ymail.com>
2018-04-28 00:07:37 +0200

    change format to use jsdoc, uploading docs folder

--- src/core/coordinates.js
+++ src/core/coordinates.js
@@ -1,11 +1,27 @@
-export default class Coordinates {
+/**
+ * class of render object's coordinates
+ */
+class Coordinates {
+	/**
+	 * create a coordinates
+	 * @param x {number}
+	 * @param y {number}
+	 */
 	constructor(x, y) {
 		this.x = x;
 		this.y = y;
 	}
 	
+	/**
+	 * set coordinates data
+	 * @param coordinates {Coordinates}
+	 * @returns {Coordinates}
+	 */
 	set(coordinates) {
 		this.x = coordinates.x;
 		this.y = coordinates.y;
+		return this;
 	}
 }
+
+export default Coordinates;
--- src/render/elementCore.js
+++ src/render/elementCore.js
@@ -1,40 +1,83 @@
-export default class ElementCore {
+/**
+ * Common element class which render on the map.
+ **/
+class ElementCore {
+	/**
+	 * Create a render element
+	 * @param props {object}
+	 */
 	constructor(props) {
 		this._ID = null;
 		this._index = 0;
 	}
 	
+	/**
+	 * set object's ID
+	 * @param ID {string}
+	 * @returns {ElementCore}
+	 */
 	setID(ID) {
 		this._ID = ID;
 		return this;
 	}
 	
+	/**
+	 * set object's render index
+	 * @param index
+	 * @returns {ElementCore}
+	 */
 	setIndex(index){
 		this._index = index;
 		return this;
 	}
 	
+	/**
+	 * get object's ID
+	 * @returns {null|string}
+	 */
 	getID() {
 		return this._ID;
 	}
 	
+	/**
+	 * get object's render index
+	 * @returns {number}
+	 */
 	getIndex(){
 		return this._index;
 	}
 	
+	/**
+	 * make render object transform by vector
+	 * @param transform {vector}
+	 * @abstract
+	 */
 	setTransform(transform){
-		//override in subClass
 	}
 	
+	/**
+	 * get render object
+	 * @return {object}
+	 * @abstract
+	 */
 	getRenderObject(){
-		//override in subClass
 	}
 	
+	/**
+	 * get render position on render canvas
+	 * @return {Coordinates}
+	 * @abstract
+	 */
 	getPosition(){
-		//override in subClass
 	}
 	
+	/**
+	 * dispose element on the map or in memory
+	 * @param option {boolean} remove from memory or not
+	 * @abstract
+	 */
 	dispose(option){
-		//override in subClass
 	}
 }
+
+export default ElementCore;
