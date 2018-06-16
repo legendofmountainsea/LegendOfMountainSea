720b849665352ca33073927cf47b09943701a730
TyrealGray <tyrealgray@ymail.com>
2018-06-16 14:36:18 +0200

    fixed issue that console.error not work with string
    Former-commit-id: b85e3736b8abadb617aecb2a8a2b4ed4c5d6bdb1

--- src/util/errorUtil.js
+++ src/util/errorUtil.js
@@ -1,4 +1,5 @@
 import {consoleAlert, consoleWarning} from '../interface/devConsole';
+import {EXECUTE_IN_CLIENT} from './envUtil';
 
 const NATIVE_MODULE_MISSING = '**LOMS** Native Module is missing! NATIVE_MODULE_MISSING';
 
@@ -22,47 +23,82 @@ export class ConfigCrashingError extends Error {
 
 export class DevConsole {
 	constructor() {
-		
-		console.warn = (warn) => {
-			consoleWarning(warn);
-		};
-		
-		console.error = (error) => {
-			if (typeof error === 'object') {
-				const errorJson = this.destroyCircular(error);
-				const errorTemplate = `error: ${errorJson.name} <br/>
+		this.waitForConsoleThenStart();
+	}
+
+	waitForConsoleThenStart() {
+		if (typeof global.window !== 'undefined') {
+
+			EXECUTE_IN_CLIENT(() => {
+				console.warn = (warn) => {
+
+					if (typeof error === 'object') {
+						const warnJson = this.destroyCircular(warn);
+						const warnTemplate = `error: ${warnJson.name} <br/>
+				info: ${warnJson.message} <br/>
+				stack: ${warnJson.stack.replace(/\n/gi, '<br/>')}`;
+						consoleWarning(warnTemplate);
+						return;
+					}
+
+					// People sometimes throw things besides Error objects, so…
+					if (typeof error === 'function') {
+						// JSON.stringify discards functions. We do too, unless a function is thrown directly.
+						consoleWarning(`[Function: ${(warn.name || 'anonymous')}]`);
+						return;
+					}
+
+					consoleWarning(warn);
+				};
+
+				console.error = (error) => {
+
+					if (typeof error === 'object') {
+						const errorJson = this.destroyCircular(error);
+						const errorTemplate = `error: ${errorJson.name} <br/>
 				info: ${errorJson.message} <br/>
 				stack: ${errorJson.stack.replace(/\n/gi, '<br/>')}`;
-				consoleAlert(errorTemplate);
-			}
-			
-			// People sometimes throw things besides Error objects, so…
-			if (typeof error === 'function') {
-				// JSON.stringify discards functions. We do too, unless a function is thrown directly.
-				consoleAlert(`[Function: ${(error.name || 'anonymous')}]`);
-			}
-		};
-		
-		window.console = console;
-		
-		consoleWarning('DevConsole on');
+						consoleAlert(errorTemplate);
+						return;
+					}
+
+					// People sometimes throw things besides Error objects, so…
+					if (typeof error === 'function') {
+						// JSON.stringify discards functions. We do too, unless a function is thrown directly.
+						consoleAlert(`[Function: ${(error.name || 'anonymous')}]`);
+						return;
+					}
+
+					consoleAlert(error);
+				};
+
+				global.window.console = console;
+
+				console.warn('DevConsole on');
+			});
+
+		} else {
+			setTimeout(() => {
+				this.waitForConsoleThenStart();
+			}, 100);
+		}
 	}
-	
+
 	destroyCircular(errorObject) {
 		const jsonObject = Array.isArray(errorObject) ? [] : {};
-		
+
 		if (typeof errorObject.name === 'string') {
 			jsonObject.name = errorObject.name;
 		}
-		
+
 		if (typeof errorObject.message === 'string') {
 			jsonObject.message = errorObject.message;
 		}
-		
+
 		if (typeof errorObject.stack === 'string') {
 			jsonObject.stack = errorObject.stack;
 		}
-		
+
 		return jsonObject;
 	}
 }
