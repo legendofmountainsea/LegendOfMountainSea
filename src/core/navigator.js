aaa41a7460c191b1f64b31dd1e425914eae1631d
TyrealGray <tyrealgray@ymail.com>
2018-06-24 18:44:53 +0200

    fixed eslint error
    Former-commit-id: 77501b10e8b62bc1116febfb6d67266fd06a82bf

--- src/core/navigator.js
+++ src/core/navigator.js
@@ -1,7 +1,7 @@
 //@flow
 import Terrain from '../render/terrain';
 
-export type NavigatorType = {
+export type NavigatorPropsType = {
 	terrain: Terrain
 };
 
@@ -9,11 +9,14 @@ export type NavigatorType = {
  * class of a navigator which provide navigation router
  */
 class Navigator {
+
+	_terrain: Terrain;
+
 	/**
 	 * create a navigator
 	 * @param props {Object}
 	 */
-	constructor(props: NavigatorType) {
+	constructor(props: NavigatorPropsType) {
 		this._terrain = props.terrain;
 	}
 }
