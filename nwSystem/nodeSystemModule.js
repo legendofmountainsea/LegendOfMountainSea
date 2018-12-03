21093ef8a4eab01dee925b41aca81088dcce495e
tyrealgray <tyrealgray@ymail.com>
2018-12-03 21:26:05 +0800

    create network module

--- nwSystem/nodeSystemModule.js
+++ nwSystem/nodeSystemModule.js
@@ -1,3 +1,7 @@
 const fileSystem = require('fs');
 const pathHelper = require('path');
 const qinjs = require('qin.js');
+const serverjs = require('loms.server').default;
+
+const lomsServer = new serverjs({});
+lomsServer.start();
